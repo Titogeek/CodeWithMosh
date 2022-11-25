c5456f4ff0fc6d4524a3177c2a60fadf79ab98bd
Titus Kodomuk <tituskodomuk420@gmail.com>
2022-11-25 22:56:24 +0300

    Try Winning

---
+++ StoreFront/Game Over.py
@@ -0,0 +1,13 @@
+# This is just for getting more focus
+
+response = input('Whats your name son? ')
+
+print(f'My name is {response}. I always play to win. May God help me to always focus')
+
+print(f'{response}. Im Calling Up To You To Focus Brother. Go Lowkey From Today And Never Flexx ')
+
+print(f'{response}. Trynna Focus For 30 Days Only. Maybe It will help you out in making new moves and habits')
+
+print(f'{response}. Some Shit That Will Help You Be Discipline And Ensure that You Achieve Your Wildest Dream ')
+
+print(f'{response}. Be Intentional About Success My Brother')
